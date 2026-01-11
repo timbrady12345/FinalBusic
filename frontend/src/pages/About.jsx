78b63ab322c5cc45079aa38ff6141522e932bab0
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2026-01-11 15:06:14 -0500

    removed example

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,30 +1,3 @@
-import { useState } from "react";
-
 export default function About() {
-  const [visible, setVisible] = useState(true);
-
-  return (
-    <div className="p-6">
-      <a
-        href="#"
-        onClick={(e) => {
-          e.preventDefault();
-          if (visible == false) {
-            setVisible(true);
-          } else {
-            setVisible(false);
-          }
-        }}
-        className="text-blue-600 underline"
-      >
-        Hide box
-      </a>
-
-      {visible && (
-        <div className="mt-4 p-4 bg-gray-200 rounded">
-          I will disappear when the link is clicked.
-        </div>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
