7d829d0913e9784a4abc4db7f3d4f0aee13f7536
Tim Brady <70961965+timbrady12345@users.noreply.github.com>
2026-01-18 12:58:50 -0500

    reset

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+
+export default function App() {
+  return (
+    <Router>
+      <nav className="sticky w-screen p-2 w-full bg-gray-900">
+        <Link to="/">Home</Link> | <Link to="/About">Facts About You</Link>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
